61a7d9c0f7df9a56902063804454708481ad796d
PabloSAlonso <santanaalonsopablo@gmail.com>
2025-04-01 12:11:54 +0200

    changes

---
+++ Ejercicios 3º Trimestre/EJERCICIOS_Usuarios.sql
@@ -0,0 +1,33 @@
+2. CREATE USER user1@localhost IDENTIFY BY 'pass'; 
+
+4. GRANT 
+
+7. SET PASSWORD FOR user1@localhost = PASSWORD('password');
+
+8. 
+
+9. 
+
+10.
+
+11.
+
+15.
+
+18.
+
+19.
+
+20.
+
+22.
+
+23.
+
+25.
+
+27.
+
+28.
+
+30.
