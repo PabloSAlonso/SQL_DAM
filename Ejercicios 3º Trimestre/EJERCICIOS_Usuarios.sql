9915fcb4fce3a4d872b7beb94a8a400c490bbf96
PabloSAlonso <santanaalonsopablo@gmail.com>
2025-04-03 12:40:55 +0200

    changes

--- Ejercicios 3º Trimestre/EJERCICIOS_Usuarios.sql
+++ Ejercicios 3º Trimestre/EJERCICIOS_Usuarios.sql
@@ -1,14 +1,14 @@
-2. CREATE USER user1@localhost IDENTIFY BY 'pass'; 
+2. CREATE USER user1@localhost IDENTIFIED BY 'pass'; 
 
 4. GRANT 
 
 7. SET PASSWORD FOR user1@localhost = PASSWORD('password');
 
-8. 
+8. GRANT SELECT(nombre, loc) ON TEMA5.depart TO user1@localhost;
 
 9. 
 
-10.
+10. 
 
 11.
 
