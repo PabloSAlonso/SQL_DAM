4459f5df63553c526c06653747a16a9712ec01e6
PabloSAlonso <santanaalonsopablo@gmail.com>
2025-05-12 12:42:21 +0200

    changes

--- Ejercicios 3º Trimestre/Transacciones.sql
+++ Ejercicios 3º Trimestre/Transacciones.sql
@@ -27,9 +27,15 @@ SELECT * FROM transacciones;
 -- No se agrega el valor por no haber realizado el COMMIT
 
 62.
+START TRANSACTION;
+INSERT INTO transacción VALUES (20);
+SELECT * FROM transacciones;
 -- No se ve el dato introducido en la conexion2
 
 63.
+START TRANSACTION;
+INSERT INTO transacción VALUES (20);
+SELECT * FROM transacciones;
 -- En la conexion1 se ve el dato ya que se ha realizado el commit
 -- En la segunda tambien se ve ya que los datos de la tabla han sido cambiados
 
@@ -65,8 +71,9 @@ INSERT INTO transacciones VALUES(110);
 SAVEPOINT primerCheck;
 INSERT INTO transacciones VALUES (111);
 CREATE DATABASE tema9;
--- Crear una base de datos se considera un commit 
--- por lo que el savepoint ya no existe
+-- Crear una base de datos tiene un commit integrado 
+-- por lo que el savepoint ya no existe por haberse 
+-- realizado la transacción
 ROLLBACK TO primerCheck;
 
 
@@ -75,8 +82,9 @@ BEGIN;
 INSERT INTO transacción VALUES(120);
 SAVEPOINT punto1;
 INSERT INTO transacción VALUES(121);
-SAVEPOINT punto2;
+SAVEPOINT punto1;
 ROLLBACK TO punto1;
+-- Vuelve al que hemos creado de ultimo porque lo sobreescribe
 INSERT INTO transacción VALUES(122);
 COMMIT;
 -- Aqui hemos salido ya de la transaccion
@@ -84,7 +92,19 @@ COMMIT;
 ROLLBACK TO punto1;
 
 69.
+LOCK TABLES transacciones READ;
+-- Podemos visualizar en las dos conexiones
+-- Al insertar en la 1 da error de que esta bloqueada
+-- Al insertar en la dos se queda a la espera
+UNLOCK TABLES;
 
 70.
+LOCK TABLES transacciones WRITE;
+-- En la 1 se puede sin problema hacer todo
+-- En la 2 nos deja a la espera cualquier consulta que hagamos
+UNLOCK TABLES;
 
 71.
+-- El primer bloqueo se hace sin problemas
+-- El segundo queda a espera por ser escritura
+-- El tercero queda a espera por estar despues del de escritura  
